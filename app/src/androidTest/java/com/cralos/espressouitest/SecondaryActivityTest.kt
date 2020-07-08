080321342367cdbe3355bc4c9d2ada44280907a9
crjcarlos1 <crjcarlos1@users.noreply.github.com>
2020-07-08 12:25:32 -0500

    Added activity-navigation-testing

--- app/src/androidTest/java/com/cralos/espressouitest/SecondaryActivityTest.kt
+++ app/src/androidTest/java/com/cralos/espressouitest/SecondaryActivityTest.kt
@@ -13,22 +13,5 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4ClassRunner::class)
 class SecondaryActivityTest{
 
-    @get :Rule
-    val activityRule = ActivityScenarioRule(SecondaryActivity::class.java)
 
-    @Test
-    fun test_isActivityInView() {
-        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
-    }
-
-    @Test
-    fun test_visibility_title_back_button() {
-        onView(withId(R.id.activity_secondary_title)).check(matches(isDisplayed()))
-        onView(withId(R.id.button_back)).check(matches(isDisplayed()))
-    }
-
-    @Test
-    fun test_isTitle_displayed() {
-        onView(withId(R.id.activity_secondary_title)).check(matches(withText(R.string.text_secondaryactivity)))
-    }
 }
