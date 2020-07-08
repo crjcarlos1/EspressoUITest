46d91a3670325c6c79247c8f1abebb78d3dd1df2
crjcarlos1 <crjcarlos1@users.noreply.github.com>
2020-07-08 12:27:06 -0500

    Added activity-navigation-testing

--- app/src/androidTest/java/com/cralos/espressouitest/MainActivityTest.kt
+++ app/src/androidTest/java/com/cralos/espressouitest/MainActivityTest.kt
@@ -5,14 +5,14 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
-class MainActivityTest{
+class MainActivityTest {
 
     @Test
     fun test_navSecondaryActivity() {
