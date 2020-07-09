f16e672a4dbc1248b773ce5dd3916826d7fd9920
crjcarlos1 <crjcarlos1@users.noreply.github.com>
2020-07-08 20:44:24 -0500

    Added custom-toast-matcher

--- app/src/androidTest/java/com/cralos/espressouitest/ui/movie/MainActivityTest.kt
+++ app/src/androidTest/java/com/cralos/espressouitest/ui/movie/MainActivityTest.kt
@@ -1,61 +1,52 @@
 package com.cralos.espressouitest.ui.movie
 
-import android.app.Activity
-import android.app.Instrumentation
-import android.content.Intent
-import android.graphics.BitmapFactory
-import android.os.Bundle
-import android.provider.MediaStore
+import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.intent.Intents.intending
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
-import androidx.test.espresso.intent.rule.IntentsTestRule
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import com.cralos.espressouitest.KEY_IMAGE_DATA
 import com.cralos.espressouitest.MainActivity
 import com.cralos.espressouitest.R
-import com.cralos.espressouitest.ui.movie.ImageViewHasDrawableMatcher.hasDrawable
-import org.hamcrest.Matcher
-import org.hamcrest.Matchers.not
-import org.junit.Rule
 import org.junit.Test
+
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
-class MainActivityTest {
+class MainActivityTest{
 
-    @get : Rule
-    val intentsTestRule = IntentsTestRule(MainActivity::class.java)
 
     @Test
-    fun test_cameraIntent_isBitmapSetToImageView() {
+    fun test_showDialog_captureNameInput() {
 
-        //Given
-        val activityResult = createImageCaptureActivityResultStub()
-        val expectedInted: Matcher<Intent> = hasAction(MediaStore.ACTION_IMAGE_CAPTURE)
-        intending(expectedInted).respondWith(activityResult)
+        // GIVEN
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        val NAME = "Mitch"
 
-        //Execute and verify
-        onView(withId(R.id.image)).check(matches(not(hasDrawable())))
-        onView(withId(R.id.button_launch_camera)).perform(click())
-        intending(expectedInted)
-        onView(withId(R.id.image)).check(matches(hasDrawable()))
+        // Execute and Verify
+        onView(withId(R.id.button_launch_dialog)).perform(click())
+
+        onView(withText(R.string.text_enter_name)).check(matches(isDisplayed()))
+
+        onView(withText(R.string.text_ok)).perform(click())
+
+        // make sure dialog is still visible (can't click ok without entering a name)
+        onView(withText(R.string.text_enter_name)).check(matches(isDisplayed()))
+
+        // enter a name
+        onView(withId(R.id.md_input_message)).perform(typeText(NAME))
+
+        onView(withText(R.string.text_ok)).perform(click())
+
+        // make sure dialog is gone
+        onView(withText(R.string.text_enter_name)).check(doesNotExist())
+
+        onView(withId(R.id.text_name)).check(matches(withText(NAME)))
+
+        //test if tost is dispaly
+        onView(withText(MainActivity.buildToastMessage(NAME))).inRoot(ToastMatcher()).check(matches(
+            isDisplayed()))
     }
-
-    private fun createImageCaptureActivityResultStub(): Instrumentation.ActivityResult? {
-        val bundle = Bundle()
-        bundle.putParcelable(
-            KEY_IMAGE_DATA,
-            BitmapFactory.decodeResource(
-                intentsTestRule.activity.resources, R.drawable.ic_launcher_background
-            )
-        )
-        val resultData = Intent()
-        resultData.putExtras(bundle)
-        return Instrumentation.ActivityResult(Activity.RESULT_OK, resultData)
-    }
-
 }
